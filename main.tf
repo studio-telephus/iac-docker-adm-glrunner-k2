b886c954f7a81550fd04508d07e8ecbc441cabac
Kristo Aun <kristoaun@live.com>
2024-01-04 02:01:35 +0200

    s3

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "container_adm_glrunner_k2" {
-  source    = "github.com/studio-telephus/terraform-lxd-instance.git?ref=1.0.1"
+  source    = "github.com/studio-telephus/terraform-lxd-instance.git?ref=1.0.3"
   name      = "container-adm-glrunner-k2"
   image     = "images:debian/bookworm"
   profiles  = ["limits", "fs-dir", "nw-adm"]
